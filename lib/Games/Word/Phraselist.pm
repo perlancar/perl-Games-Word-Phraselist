8497cf32a77bce581582b516c0763595f685b101
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2014-08-10 21:41:52 +0700

    Initial commit

---
+++ lib/Games/Word/Phraselist.pm
@@ -0,0 +1,10 @@
+package Games::Word::Phraselist;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use parent qw(Games::Word::Wordlist);
+
+1;
+# ABSTRACT: Manage a list of phrases
---
+++ t/01-basics.t
@@ -0,0 +1,11 @@
+#!perl
+
+use 5.010;
+use strict;
+use warnings;
+
+use Games::Word::Phraselist;
+use Test::More 0.98;
+
+ok 1; # TODO
+done_testing;
